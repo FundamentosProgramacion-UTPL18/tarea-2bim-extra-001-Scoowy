ad9a706dab9962d519925ac3d9b3e84877549be8
Scoowy <gahonajuanjo@gmail.com>
2018-06-01 09:28:38 -0500

    Add Coments - Change var names

--- Tarea1/src/tarea1/Tarea1.java
+++ Tarea1/src/tarea1/Tarea1.java
@@ -13,40 +13,41 @@ public class Tarea1 {
         // Declaramos las variables
         String[] meses = {"Agosto", "Octubre", "Diciembre"};
         String[] sucursales = {"sucursal 1", "sucursal 2", "sucursal 3", "sucursal 4"};
-        double[] ventas_mes_1 = {4500.2, 5500.2, 6500.2, 2500.1};
-        double[] ventas_mes_2 = {1500.2, 2500.2, 3500.2, 1500.1};
-        double[] ventas_mes_3 = {2300.2, 1200.2, 4700.2, 3200.1};
+        double[] ventasMes1 = {4500.2, 5500.2, 6500.2, 2500.1};
+        double[] ventasMes2 = {1500.2, 2500.2, 3500.2, 1500.1};
+        double[] ventasMes3 = {2300.2, 1200.2, 4700.2, 3200.1};
 
-        double[] venta_total_sucursal = new double[4];
-        double[] venta_promedio_sucursal = new double[4];
-        
-        double total_ventas = 0;
+        double[] ventaTotalSucursal = new double[4];
+        double[] ventaPromSucursal = new double[4];
+
+        double totalVentas = 0;
 
         /**
-         * Estructura de columnas 1 Col1 Col2 Col3 Col4 Col5 Col6 %-15s %-15s
-         * %-15s %-15s %-15s %-15s
+         * Estructura de columnas 1 
+         * Col1  Col2  Col3  Col4  Col5  Col6 
+         * %-15s %-15s %-15s %-15s %-15s %-15s
          */
         String reporteGeneral = String.format("Reporte\n\n%-15s%-15s%-15s%-15s%-15s%-15s\n\n", "", meses[0], meses[1], meses[2], "Total", "Promedio");
 
         for (int i = 0; i < sucursales.length; i++) {
             // Calculamos el total de cada sucursal
-            venta_total_sucursal[i] = ventas_mes_1[i] + ventas_mes_2[i] + ventas_mes_3[i];
+            ventaTotalSucursal[i] = ventasMes1[i] + ventasMes2[i] + ventasMes3[i];
             // Calculamos el promedio de cada sucursal
-            venta_promedio_sucursal[i] = venta_total_sucursal[i] / meses.length;
+            ventaPromSucursal[i] = ventaTotalSucursal[i] / meses.length;
             /**
              * Estructura de columnas 2
              * Col1  Col2    Col3    Col4    Col5    Col6
              * %-15s %-15.2f %-15.2f %-15.2f %-15.2f %-15.2f
              */
-            reporteGeneral = String.format("%s%-15s%-15.2f%-15.2f%-15.2f%-15.2f%-15.2f\n", reporteGeneral, sucursales[i], ventas_mes_1[i], ventas_mes_2[i], ventas_mes_3[i], venta_total_sucursal[i], venta_promedio_sucursal[i]);
+            reporteGeneral = String.format("%s%-15s%-15.2f%-15.2f%-15.2f%-15.2f%-15.2f\n", reporteGeneral, sucursales[i], ventasMes1[i], ventasMes2[i], ventasMes3[i], ventaTotalSucursal[i], ventaPromSucursal[i]);
         }
         // Calculamos el total de ventas de tosas las sucursales
-        for (int i = 0; i < venta_total_sucursal.length; i++) {
-            total_ventas += venta_total_sucursal[i];
+        for (int i = 0; i < ventaTotalSucursal.length; i++) {
+            totalVentas += ventaTotalSucursal[i];
         }
 
-        reporteGeneral = String.format("%s\nTotales de ventas de todas la sucursales es: %.2f", reporteGeneral, total_ventas);
-        
+        reporteGeneral = String.format("%s\nTotales de ventas de todas la sucursales es: %.2f", reporteGeneral, totalVentas);
+
         // Imprimimos el reporte final
         System.out.println(reporteGeneral);
     }
